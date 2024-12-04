fa51ec309e83cc6c158eb3f467b76d12a3ba516a
lioneltarun10 <lioneltarun10@gmail.com>
2024-12-04 20:41:28 +0530

    Episode-03

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/**
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *      </div>
- * <div>
- */
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [React.createElement(
-        "div",
-        {id:"child"}, [
-            "I'm a text",
-            React.createElement("h1",{},"This is namaste react 🚀"),
-            React.createElement("h2",{},"I'm an h2 tag")
-        ]
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"}, [
-            React.createElement("h1",{},"I'm an h1 tag"),
-            React.createElement("h2",{},"I'm an h2 tag")
-        ]
-    )]
-);
+const elem = <div>React Element</div>
 
-// const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
-    
-console.log(parent);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(parent);
+
+// JSX - is not HTML in JS , its a HTML like syntax
+const title = (
+  
+<h1 className="head" tabIndex="5">
+ {elem} Namaste react using JSX
+    </h1>
+    )
+
+
+// Component Composition
+const HeadingComponent = () => (
+    <div id="container">  
+    {title}
+        <h1 className="heading">Namaste React Functional Component</h1>
+   </div>
+)
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent></HeadingComponent>)
